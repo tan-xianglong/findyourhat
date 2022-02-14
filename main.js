f52c4ac6319603445812f70318d8645603d92430
tan-xianglong <t.xianglong@gmail.com>
2022-02-14 17:19:48 +0800

    added various helper functions - restart, quit, game over, update player coordinates, move player

--- main.js
+++ main.js
@@ -14,6 +14,7 @@
     iv) if value is ░ then update value to be *
 
 5. may want to offer option to reset game and generate new field --> after mvp is done
+6. may ask for user name and address user by name and give explanation to the game --> after mvp is done
 */
 
 const prompt = require("prompt-sync")({ sigint: true });
@@ -26,6 +27,12 @@ const fieldCharacter = "░";
 const pathCharacter = "*";
 const row = 10;
 const col = 10;
+const up = 'U';
+const down = 'D';
+const left = 'L';
+const right = 'R';
+const quit = 'QUIT';
+const restart = 'RESTART';
 
 //create Field Class for generating field
 class Field {
@@ -87,11 +94,89 @@ class Field {
   askQuestion() {
     const answer = prompt(`Which way? `).toUpperCase();
     //implement your codes
-
+    this.validateInput(answer);
+    this.updatePlayerCoordinates(answer);
+    this.movePlayer();
     this.runGame();
   }
 
-  
+  //method to validate input
+  validateInput(answer){
+    const validInput = ['U', 'D', 'L', 'R', 'QUIT', 'RESTART'];
+    if (!validInput.includes(answer)){
+      this.print();
+      console.log(`Please kindly input U, D, L, R, Quit or Restart.`)
+      this.askQuestion();
+      return;
+    }
+  }
+
+  //method to take input and translate it into player movement and update player coordinates
+  updatePlayerCoordinates(answer){
+    switch(answer){
+      case up:
+        this.locationY -= 1;
+        break;
+      case down:
+        this.locationY += 1;
+        break;
+      case left:
+        this.locationX -= 1;
+        break;
+      case right:
+        this.locationX += 1;
+        break;
+      case quit:
+        this.quitGame();
+      case restart:
+        this.restartGame();
+    }
+  }
+
+  //method to restart game
+  restartGame(){
+    this.generateField(row, col, 0.2);
+    this.locationX = 0;
+    this.locationY = 0;
+    this.runGame();
+    return;
+  }
+
+  //method to exit game
+  quitGame(){
+    console.log("Thank you, have a nice day. Good bye");
+    return process.exit();
+  }
+
+  //method for Game Over
+  gameOver(){
+    const quitOrRestart = prompt(`Would you like to restart or quit?`).toUpperCase();
+    const validInput = ["QUIT", "RESTART"];
+    //input validation
+    if(!validInput.includes(quitOrRestart)){
+      this.print();
+      console.log(`Game has ended, please kindly input Quit or Restart.`);
+      this.gameOver();
+    } else if (quitOrRestart == quit){
+      // player to choose to restart or exit game
+      this.quitGame();
+    } else if (quitOrRestart == restart) {
+      this.restartGame();
+    }
+  }
+
+  //method to access field array and determine if player can occupy field new position or end game or win the game
+  movePlayer() {
+    if (
+      this.field[this.locationY][this.locationX] == fieldCharacter ||
+      this.field[this.locationY][this.locationX] == pathCharacter
+    ) {
+      this.field[this.locationY][this.locationX] = pathCharacter;
+    } else if (this.field[this.locationY][this.locationX] == hole) {
+      console.log("Oh no! You have fallen into the hole. Game Over.")
+      this.gameOver();
+    }
+  }
 
 } // End of Class
 
