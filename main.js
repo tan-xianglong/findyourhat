dba7138cabc822690e4a16ad8968eb8146857b5b
tan-xianglong <t.xianglong@gmail.com>
2022-02-14 15:13:18 +0800

    add pseudo codes on approach to design game

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+/* Game design step by step
+1. Design the field generation with holes
+  a. if prob > percentage, then assign fieldCharacter. if prob < percentage, then assign hole to fieldArray
+2. randomize hat location within the 10 x 10 and access the field array to reassign symbol to "^"
+3. set character position to [0][0] and access field array to reassign symbol to "*" --> may consider randomize character position once MVP is done
+4. tracking character movement:
+  a. assign input values of up down left right with + or - 1
+  b. update the locationX and locationY values per user input
+  c. conduct input validation with error msg shown if input is not part of the validation array.
+  d. based on the location X and Y, access the field array to see what is the value
+    i) if value is ^, show win msg
+    ii) if value is 0, show lose msg
+    iii) if location X and Y value is negative value or more than 9, show lose msg and end game.
+    iv) if value is ░ then update value to be *
+
+5. may want to offer option to reset game and generate new field --> after mvp is done
+*/
+
 const prompt = require("prompt-sync")({ sigint: true });
 const clear = require("clear-screen");
 
@@ -8,6 +26,7 @@ const fieldCharacter = "░";
 const pathCharacter = "*";
 const row = 10;
 const col = 10;
+
 
 //create Field Class for generating field
 class Field {
@@ -29,7 +48,9 @@ class Field {
     for (let y = 0; y < height; y++) {
       for (let x = 0; x < width; x++) {
         const prob = Math.random();
-        this.field[y][x] = fieldCharacter;
+        if(prob > percentage) {
+          this.field[y][x] = fieldCharacter;
+        }
       }
     }
     // Set the "hat" location
