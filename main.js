4160814ea1bad73560281b43d77d70443ceb7334
tan-xianglong <t.xianglong@gmail.com>
2022-02-14 14:07:43 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")({ sigint: true });
+const clear = require("clear-screen");
+
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
+const row = 10;
+const col = 10;
+
